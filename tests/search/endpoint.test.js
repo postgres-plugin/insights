7addf33ef1f4446526aad93f378c579d3c9dce77
simonLab <simon.labondance@gmail.com>
2017-10-03 13:07:36 +0100

    do not search for insights resource type

--- tests/search/endpoint.test.js
+++ tests/search/endpoint.test.js
@@ -6,7 +6,7 @@ var config = require('../../config/load-config.js');
 
 test('search for insights containing searchTerm in title', function (t) {
   init(config, function (err, server, pool) {
-    server.inject({ url: '/insightsSearch?searchTerm=insight number 1' }, function (res) {
+    server.inject({ url: '/insightsSearch?searchTerm=insight number 3' }, function (res) {
       t.equal(res.result.length, 1, 'Insights Number 1 found');
       t.end();
       pool.end();
@@ -17,7 +17,7 @@ test('search for insights containing searchTerm in title', function (t) {
 
 test('search for insights containing searchTerm in author', function (t) {
   init(config, function (err, server, pool) {
-    server.inject({ url: '/insightsSearch?searchTerm=jones' }, function (res) {
+    server.inject({ url: '/insightsSearch?searchTerm=jennifer' }, function (res) {
       t.equal(res.result.length, 1, 'insights found by author');
       t.end();
       pool.end();
@@ -28,7 +28,7 @@ test('search for insights containing searchTerm in author', function (t) {
 
 test('search for insights containing searchTerm in tags', function (t) {
   init(config, function (err, server, pool) {
-    server.inject({ url: '/insightsSearch?searchTerm=agriculture' }, function (res) {
+    server.inject({ url: '/insightsSearch?searchTerm=luxembourg' }, function (res) {
       t.equal(res.result.length, 1, 'insights found by tags');
       t.end();
       pool.end();
